b2a08f3872153885a3738c0391ad129752482961
Dustin Wilson <dustin52@users.campus>
2019-09-09 17:44:59 -0500

    Added Brontowurst and DinoNuggets

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        public double Price { get; set; }
+
+        public uint Calories { get; set; }
+
+        private List<string> ingredients = new List<string>() { "Chicken Nugget", "Chicken Nugget", "Chicken Nugget", "Chicken Nugget", "Chicken Nugget", "Chicken Nugget" };
+
+        public List<string> Ingredients { get; }
+
+        public DinoNuggets()
+        {
+            Price = 4.25;
+            Calories = 59 * 6;
+        }
+
+        public void AddNugget()
+        {
+            Price += .25;
+            Calories += 59;
+            ingredients.Add("Chicken Nugget");
+        }
+    }
+}
