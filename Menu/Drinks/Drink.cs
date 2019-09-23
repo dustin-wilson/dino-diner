3f2ddffc81e08db268652c8ae692de9c3517e7b8
Dustin Wilson <dustin52@users.campus>
2019-09-23 17:07:15 -0500

    Started Milestone 3

---
+++ Menu/Drinks/Drink.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu.Sides;
+
+namespace DinoDiner.Menu.Drinks
+{
+    class Drink
+    {
+        protected List<string> ingredients = new List<string>(); 
+
+        /// <summary>
+        /// Gets or sets the size
+        /// </summary>
+        public virtual Size Size { get; set; }
+
+        /// <summary>
+        /// Gets or sets the price
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets the calories
+        /// </summary>
+        public uint Calories { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ingredients
+        /// </summary>
+        public virtual List<string> Ingredients { get { return ingredients; } }
+
+        /// <summary>
+        /// Indicates whether ice should be added
+        /// </summary>
+        public bool Ice { get; set; } = true;
+
+        /// <summary>
+        /// Sets it so ice will not be an added ingredient
+        /// </summary>
+        public void HoldIce()
+        {
+            Ice = false;
+        }
+    }
+}
---
+++ Menu/Drinks/Sodasaurus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DinoDiner.Menu.Sides;
+
+namespace DinoDiner.Menu.Drinks
+{
+    public enum SodasaurusFlavor
+    {
+        Cola,
+        Orange,
+        RootBeer,
+        Lime,
+        Cherry,
+        Vanilla,
+        Chocolate
+    }
+    class Sodasaurus : Drink
+    {
+        private Size size;
+
+        SodasaurusFlavor Flavor { get; set; }
+
+        public override Size Size
+        {
+            get { return size; }
+
+            set
+            {
+                size = value;
+
+                switch (size)
+                {
+                    case Size.Small:
+                        Price = 1.5;
+                        Calories = 112;
+                        break;
+                    case Size.Medium:
+                        Price = 2;
+                        Calories = 156;
+                        break;
+                    case Size.Large:
+                        Price = 2.5;
+                        Calories = 208;
+                        break;
+                }
+            }
+        }
+
+        public Sodasaurus()
+        {
+            this.Calories = 112;
+            this.Price = 1.5;
+
+            ingredients.Add("Water");
+            ingredients.Add("Cane Sugar");
+        }
+    }
+}
